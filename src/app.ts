a17743f148152cbeb9799296826fd69693bd35e4
jyyoo <jyyoo@autocrypt.io>
2023-01-23 01:28:35 +0900

    test middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
 import * as express from "express";
+import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 
 const port: number = 8000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  console.log({ req });
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is a middleware for logging");
+  next();
+});
 
-  res.send({ name: "yoo joyoung", age: 99, family: ["wan", "ok", "hyuk"] });
+app.get("/cat/som", (req, res, next) => {
+  console.log("this is a middleware only for /cat/som routes");
+  next();
+});
+
+app.get("/cat/som", (req: express.Request, res: express.Response) => {
+  // console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat, blue: Cat[1] });
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat });
+});
+
+app.get("/cat/blue", (req: express.Request, res: express.Response) => {
+  // console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat, blue: Cat[0] });
 });
 
 app.listen(port, () => {
