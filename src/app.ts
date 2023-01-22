088482305915e82147f4ce57bed5ab9dac3fdf9b
jyyoo <jyyoo@autocrypt.io>
2023-01-23 02:15:35 +0900

    모듈분리 및 UD

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import * as express from "express";
-import { Cat, CatType } from "./app.model";
+import catRouter from "./cats/cats.routes";
 
 const app: express.Express = express();
-
 const port: number = 8000;
 
 // logging middleward
@@ -15,55 +14,7 @@ app.use((req, res, next) => {
 // middleware for requestBody
 app.use(express.json());
 
-// 고양이 전체 데이터 조회
-app.get("/cats", (req, res) => {
-  try {
-    const cats = Cat;
-    // throw new Error("db connection error");
-    res.status(200).send({
-      success: true,
-      data: cats,
-    });
-  } catch (error) {
-    res.status(500).send({ success: false, error: "" });
-  }
-});
-
-// 특정 고양이 데이터 조회
-app.get("/cat/:id", (req, res) => {
-  try {
-    const params = req.params;
-    console.log({ params });
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-
-    res.status(200).send({
-      success: true,
-      data: cat,
-    });
-  } catch (error) {
-    res.status(500).send({ success: false, error: "" });
-  }
-});
-
-// 고양이 추가
-app.post("/cat", (req, res) => {
-  try {
-    const body = req.body;
-    console.log({ body });
-
-    Cat.push(body);
-    res.status(200).send({
-      success: true,
-      data: Cat,
-    });
-  } catch (error) {
-    res.status(500).send({ success: false, error: "" });
-  }
-});
-
-//express 에서는 RequestBody 를 읽기 위한 middleware 추가
+app.use(catRouter);
 
 // not found middleward
 app.use((req, res, next) => {
---
+++ src/cats/cats.routes.ts
@@ -0,0 +1,107 @@
+// 고양이 전체 데이터 조회
+import { Cat, CatType } from "./cats.model";
+import { Router } from "express";
+const router = Router();
+
+// 모든 고양이 조회
+router.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connection error");
+    res.status(200).send({
+      success: true,
+      data: cats,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, error: "" });
+  }
+});
+
+// 특정 고양이 데이터 조회
+router.get("/cat/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log({ params });
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+
+    res.status(200).send({
+      success: true,
+      data: cat,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, error: "" });
+  }
+});
+
+//* UPDATE 고양이 데이터 업데이트 -> PUT
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: "",
+    });
+  }
+});
+
+//* UPDATE 고양이 데이터 부분적으로 업데이트 -> PATCH
+router.patch("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: "",
+    });
+  }
+});
+
+//* DELETE 고양이 데이터 삭제 -> DELETE
+router.delete("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+    res.status(200).send({
+      success: true,
+      data: newCat,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: "",
+    });
+  }
+});
+
+export default router;
