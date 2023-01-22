49b02a83a299f5df88d85e9cb5f6dd3ec57b8b09
jyyoo <jyyoo@autocrypt.io>
2023-01-23 01:55:09 +0900

    고양이 정보 CR

--- src/app.ts
+++ src/app.ts
@@ -5,30 +5,70 @@ const app: express.Express = express();
 
 const port: number = 8000;
 
+// logging middleward
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   console.log("this is a middleware for logging");
   next();
 });
 
-app.get("/cat/som", (req, res, next) => {
-  console.log("this is a middleware only for /cat/som routes");
+// middleware for requestBody
+app.use(express.json());
+
+// 고양이 전체 데이터 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connection error");
+    res.status(200).send({
+      success: true,
+      data: cats,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, error: "" });
+  }
+});
+
+// 특정 고양이 데이터 조회
+app.get("/cat/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log({ params });
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+
+    res.status(200).send({
+      success: true,
+      data: cat,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, error: "" });
+  }
+});
+
+// 고양이 추가
+app.post("/cat", (req, res) => {
+  try {
+    const body = req.body;
+    console.log({ body });
+
+    Cat.push(body);
+    res.status(200).send({
+      success: true,
+      data: Cat,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, error: "" });
+  }
+});
+
+//express 에서는 RequestBody 를 읽기 위한 middleware 추가
+
+// not found middleward
+app.use((req, res, next) => {
+  console.log("404 not found");
   next();
-});
-
-app.get("/cat/som", (req: express.Request, res: express.Response) => {
-  // console.log(req.rawHeaders[1]);
-  res.send({ cats: Cat, blue: Cat[1] });
-});
-
-app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
-  res.send({ cats: Cat });
-});
-
-app.get("/cat/blue", (req: express.Request, res: express.Response) => {
-  // console.log(req.rawHeaders[1]);
-  res.send({ cats: Cat, blue: Cat[0] });
 });
 
 app.listen(port, () => {
