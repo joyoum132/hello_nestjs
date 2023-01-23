957d62edeea4f0752fc78d02a7cb2c6fd6ab8ee7
jyyoo <jyyoo@autocrypt.io>
2023-01-23 16:14:19 +0900

    response 에 middleware 추가하기

--- project/src/logger/logger.middleware.ts
+++ project/src/logger/logger.middleware.ts
@@ -6,7 +6,12 @@ export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
 
   use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(`${req.ip} ${req.method} ${req.originalUrl}`);
+    // finish 는 event 이름 : response 가 완료되었을때
+    res.on('finish', () => {
+      this.logger.log(
+        `${req.ip} || ${req.method} || ${req.originalUrl} || ${res.statusCode}`,
+      );
+    });
     next();
   }
 }
