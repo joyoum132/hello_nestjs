ce996e3391fbfdd6d64521fcce8b1102e3479fb0
jyyoo <jyyoo@autocrypt.io>
2023-01-23 14:49:51 +0900

    데코레이터 패턴

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // 데코레이터 패턴
+  // body 와 param 은 req 안에서 모두 가져올 수 있지만 데코레이터를 사용해서 명시적으로 받을 수 있음
   @Get()
   getHello(
     @Req() req: Request,
